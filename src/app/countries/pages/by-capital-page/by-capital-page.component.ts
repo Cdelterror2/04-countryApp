2d16d03111aa6c916081e28f7228c1e3ee71adb8
Carolina Bolivar <cdelterror123@gmail.com>
2024-10-24 18:56:56 -0500

    cap-123

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-capital-page',
-  templateUrl: './by-capital-page.component.html',
-  styles: ``
+  templateUrl: './by-capital-page.component.html'
 })
 export class ByCapitalPageComponent {
 
+  constructor(private CountriesService: CountriesService){
+
+  }
+
+  searchByCapital(term: string): void {
+    this.CountriesService.searchCapital(term);
+  }
 }
