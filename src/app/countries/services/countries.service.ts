19ef461d00feeabf300339c1a642cad582197458
Carolina Bolivar <cdelterror123@gmail.com>
2024-10-25 15:49:37 -0500

    cap-126

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -11,6 +11,9 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapital(term: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+      .pipe(
+        catchError( () => of([]))
+        );
   }
 }
