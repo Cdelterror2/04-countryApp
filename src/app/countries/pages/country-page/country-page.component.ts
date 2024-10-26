dbbcc4b96e418f92ec9d3e73025449ebfa3f2c99
Carolina Bolivar <cdelterror123@gmail.com>
2024-10-25 20:35:38 -0500

    cap-130

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,9 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit{
+
+  public country? : Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router : Router,
@@ -26,8 +30,7 @@ export class CountryPageComponent implements OnInit{
       if (!country){
         return this.router.navigateByUrl('');
       }
-      console.log('Tenemos un país');
-      return;
+      return this.country = country;
       });
   }
 
